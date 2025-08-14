6882ad0b599888cd839d0ff43ab75e4da935d997
farhan <your-username@users.noreply.github.com>
2025-08-14 11:34:16 +0100

    changed the lgoo location v2

--- src/Concept.jsx
+++ src/Concept.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import CorexaLogo from 'Corexa_logo.png'
+import CorexaLogo from '/Corexa_logo.png';
 
 function Concept() {
   const [isVisible, setIsVisible] = useState(false)
